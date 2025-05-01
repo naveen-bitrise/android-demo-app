554c011c18e862538e1a36c8debd2bb4d4a16886
Naveen Nazimudeen <naveen.nazimudeen@bitrise.io>
2025-05-01 15:56:56 -0400

    added a test case that sleeps for 10 mts

--- app/src/androidTest/java/io/bitrise/sample/android/instrumentation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/bitrise/sample/android/instrumentation/ExampleInstrumentedTest.kt
@@ -21,4 +21,20 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("io.bitrise.sample.android", appContext.packageName)
     }
+
+    @Test
+    fun longRunningTest() {
+        // Test that sleeps for 10 minutes
+        println("Starting 10-minute sleep test")
+        try {
+            // Sleep for 10 minutes (600,000 milliseconds)
+            Thread.sleep(TimeUnit.MINUTES.toMillis(10))
+            println("10-minute sleep completed")
+        } catch (e: InterruptedException) {
+            println("Sleep was interrupted: ${e.message}")
+            throw e
+        }
+        // Simple assertion to make sure the test passes after sleeping
+        assertTrue(true)
+    }
 }
